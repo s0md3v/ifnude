e1e1d7a0dc81f617a3d3a7d84de394a45310364c
Somdev Sangwan <s0md3v@gmail.com>
2023-06-19 03:24:35 +0530

    fix dependencies

--- setup.py
+++ setup.py
@@ -15,13 +15,13 @@ URL = 'https://github.com/s0md3v/ifnude'
 EMAIL = 's0md3v@gmail.com'
 AUTHOR = 'Somdev Sangwan'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
     'pillow',
     'opencv-python-headless>=4.5.1.48',
-    'pydload',
+    'tqdm',
     'scikit-image',
     'onnxruntime'
 ]
